61f6afaf04d163c12c88ed1aafeeabd91700501a
Christos Giachoudis <46033926+giahou2000@users.noreply.github.com>
2025-02-11 14:13:30 +0100

    Add files via upload

---
+++ baseline_analysis.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+
+echo "Hello World"
+
+CastaliaResults -i mobile6Baseline.txt -s "Application level latency" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "Energy" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "Packets loss rate" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "Packets received per node" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "Packets reception rate" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "Beacons received" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "Beacons sent" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "Data pkt breakdown" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "Mgmt & Ctrl pkt breakdown" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "pkt TX state breakdown" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "RX pkt breakdown" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "TXed pkts" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "Estimated network lifetime" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "Execution ratio" -n >> mobile6Baseline_analysis.txt
+CastaliaResults -i mobile6Baseline.txt -s "Execution time" -n >> mobile6Baseline_analysis.txt
---
+++ zigbee_analysis.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+
+echo "Hello World"
+
+CastaliaResults -i mobile12Zigbee.txt -s "Application level latency" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "Energy" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "Packets loss rate" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "Packets received per node" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "Packets reception rate" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "Beacons received" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "Beacons sent" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "Fraction of time without PAN connection" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "Packet breakdown" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "pkt TX state breakdown" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "RX pkt breakdown" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "TXed pkts" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "Estimated network lifetime" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "Execution ratio" -n >> mobile12Zigbee_analysis.txt
+CastaliaResults -i mobile12Zigbee.txt -s "Execution time" -n >> mobile12Zigbee_analysis.txt
